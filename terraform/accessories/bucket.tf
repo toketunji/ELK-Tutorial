b82372031958f91127e517cc7fd88d9bfe9c044d
Teppy Oketunji <toketunji@gmail.com>
2018-07-22 23:16:20 +0100

    updated Jenkinsfiles

--- terraform/accessories/bucket.tf
+++ terraform/accessories/bucket.tf
@@ -14,11 +14,11 @@ resource "aws_s3_bucket" "terraform-state-storage-s3" {
     }
 }
 
-provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "eu-west-1"
-}
+#provider "aws" {
+#  access_key = "${var.aws_access_key}"
+#  secret_key = "${var.aws_secret_key}"
+#  region     = "eu-west-1"
+#}
 
 
 
