de673c088d5e2151ebc0b3489eb0e3ba099542f4
Teppy Oketunji <toketunji@gmail.com>
2018-07-23 19:18:32 +0100

    update files

--- terraform/ELK-Artefacts/data_source.tf
+++ terraform/ELK-Artefacts/data_source.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "network_info" {
 
   config {
     encrypt = "true"
-    bucket  = "tealbucket1"
+    bucket  = "buc003"
     key     = "Network_routing/terraform.tfstate"
     region  = "eu-west-1"
     profile = "lab"
